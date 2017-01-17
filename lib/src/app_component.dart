165097bf3143571bdbf371f9933d326b38067e9e
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-01-17 15:11:55 +0300

    alert component added

--- lib/src/app_component.dart
+++ lib/src/app_component.dart
@@ -5,35 +5,37 @@ import 'package:angular2/router.dart';
 //import 'package:cmas/routes.dart';
 import 'package:auth/auth_service.dart';
 import 'package:auth/auth_component.dart';
-
+import 'package:alert/alert_service.dart';
+import 'package:alert/alert_component.dart';
 
 @Component(
     selector: 'cmas-app',
-    /*providers: const [
-      AlertService
-    ]*/
+
     providers: const [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(AuthenticationService, useClass: AuthenticationService),
+    const Provider(AlertService, useClass: AlertService),
     ]
 )
 
 @View(
     templateUrl: 'app_component.html',
     styleUrls: const ['app_component.css'],
-    directives: const [RouterLink, /*AlertComponent,*/ RouterOutlet],
+    directives: const [RouterLink, AlertComponent, RouterOutlet],
 
 )
 
 @RouteConfig(const [AuthComponent.route])
 
 class AppComponent implements  AfterViewInit {
+  final AlertService _alertService;
 
-  AppComponent() {
+  AppComponent(this._alertService) {
 
   }
 
   ngAfterViewInit() {
+    _alertService.Info('ngAfterViewInit!');
   }
 }
