4daba871c652ec872edd7095bbb96fb7930d97c7
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-21 09:30:37 +0300

    global exceptions handler

--- web/main.dart
+++ web/main.dart
@@ -1,25 +1,15 @@
 import 'dart:core';
-
+import 'dart:async';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/src/core/reflection/reflection.dart';
 
 import 'package:cmas/app_component.dart';
 
-bool get isDebug =>
-    (const String.fromEnvironment("PRODUCTION", defaultValue: "false")) !=
-    "true";
-
 main() async {
-  if (isDebug) {
-    reflector.trackUsage();
-  }
-
-  ComponentRef ref = await bootstrap(AppComponent);
-
-  if (isDebug) {
-    print("Application in DebugMode");
-    enableDebugTools(ref);
-    print('Unused keys: ${reflector.listUnusedKeys()}');
-  }
+  runZoned(() {
+    bootstrap(AppComponent);
+  }, onError: (error, stackTrace) {
+    print('Uncaught error: $error');
+  });
 }
