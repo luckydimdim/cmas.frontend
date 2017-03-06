e01f221bd89c36212f97c21da59678a3f0736bad
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-03-06 10:09:43 +0300

    client provider

--- web/main.dart
+++ web/main.dart
@@ -4,7 +4,6 @@ import 'package:angular2/core.dart';
 import 'package:angular2/src/core/reflection/reflection.dart';
 import 'package:cmas/app_component.dart';
 import 'package:cmas/app_exception_handler.dart';
-import 'package:logger/logger_service.dart';
 
 bool get isDebug =>
     (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) !=
